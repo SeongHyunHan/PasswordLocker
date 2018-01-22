81909a0a388306ddd695abae0dfba9190664d512
Seong Han <hans@marc-hi.ca>
2018-01-22 16:30:56 -0500

    Implement mongoose

--- mongodb/db.js
+++ mongodb/db.js
@@ -1,33 +1,9 @@
 const {MongoClient, ObjectID} = require('mongodb');
+const mongoose = require('mongoose');
 
-var getAllPassword = () =>{
-    MongoClient.connect('mongodb://localhost:27017/PasswordLocker', (err, db) => {
-        if(err){
-            console.log('Unable to connect to MongoDB server');
-        }else{
-            console.log('Connected to MongoDB server');
-            const myDb = db.db('PasswordLocker');
-
-            myDb.collection('Passwords').find().toArray().then((docs) => {
-                console.log('Able to get all data from Password documents')
-                console.log(JSON.stringify(docs, undefined, 2));
-                return docs
-            }, (err) => {
-                console.log('Unable to ftech Passwords', err);
-            });
-
-        }
-    });
-};
-
-
-
-
-
-
-
-
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/PasswordLocker');
 
 module.exports = {
-    getAllPassword
-}
+    mongoose
+};
